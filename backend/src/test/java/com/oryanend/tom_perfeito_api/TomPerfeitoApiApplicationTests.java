5f7c8d4eb061ac4e89c33f4e29640e495acb0049
oryanend <ryanyamim@gmail.com>
2026-01-10 21:00:53 -0300

    ci: create `linting.yml` and `test.yml`

--- backend/src/test/java/com/oryanend/tom_perfeito_api/TomPerfeitoApiApplicationTests.java
+++ backend/src/test/java/com/oryanend/tom_perfeito_api/TomPerfeitoApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TomPerfeitoApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
