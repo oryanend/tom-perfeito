1499534f76f0d193065601b1f8df101625b27e5e
oryanend <ryanyamim@gmail.com>
2026-02-15 22:36:11 -0300

    feat: create `waiting-page`

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WaitingPageComponent } from './waiting-page/waiting-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WaitingPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/waiting-page/waiting-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-waiting-page',
+  standalone: false,
+  templateUrl: './waiting-page.component.html',
+  styleUrl: './waiting-page.component.css'
+})
+export class WaitingPageComponent {
+
+}
