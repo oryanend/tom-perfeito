f8996e23e7ba59d7aff018ace7a1cb3f0c46fa88
oryanend <ryanyamim@gmail.com>
2026-02-23 22:47:59 -0300

    feat: create first version `navbar`

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WaitingPageComponent } from './waiting-page/waiting-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent },
+  { path: '', component: WaitingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
