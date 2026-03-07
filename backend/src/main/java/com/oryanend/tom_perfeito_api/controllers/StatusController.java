6bb70c1e0ff61eaeaf0f853bb030a14c6ea39479
oryanend <ryanyamim@gmail.com>
2026-03-07 20:24:49 -0300

    feat: add `/status` endpoint

--- backend/src/main/java/com/oryanend/tom_perfeito_api/controllers/StatusController.java
+++ backend/src/main/java/com/oryanend/tom_perfeito_api/controllers/StatusController.java
@@ -1,4 +1,21 @@
 package com.oryanend.tom_perfeito_api.controllers;
 
+import com.oryanend.tom_perfeito_api.services.StatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/status")
 public class StatusController {
+    @Autowired
+    private StatusService service;
+
+    @GetMapping
+    public Map<String, Object> getStatus() {
+        return service.getStatus();
+    }
 }
