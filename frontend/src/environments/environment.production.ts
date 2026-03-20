54dfac1959ec5beda126c7829538d5415c9325cb
oryanend <ryanyamim@gmail.com>
2026-03-19 22:07:34 -0300

    chore: update `environment.production.ts` api url to `Azure` deploy

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tom-perfeito-api-3e1f688c2104.herokuapp.com',
+  apiUrl: 'https://tom-perfeito-api-bmagenbthkdcbfhn.brazilsouth-01.azurewebsites.net',
 };
