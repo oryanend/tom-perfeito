9651336eaab74ca65e5ccf5677f9ff72c8724f41
oryanend <ryanyamim@gmail.com>
2026-04-11 16:28:00 -0300

    feat: addd author and time that music was created in `/music` description

--- frontend/src/app/shared/models/music.ts
+++ frontend/src/app/shared/models/music.ts
@@ -1,6 +1,10 @@
+import { UserMin } from './user-min';
+
 export interface Music {
   id: string;
   title: string;
   description: string;
+  createdAt: string;
+  createdBy: UserMin;
   link: string;
 }
